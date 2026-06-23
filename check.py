e0ca19ec63d51cc73ee0e703eda835e8ebb44be4
onlinetochilka <onlinetochilka@gmail.com>
2026-06-24 01:38:23 +0300

    chore: remove temp scripts

--- check.py
+++
@@ -1,7 +0,0 @@
-import re
-app = open('app.js', encoding='utf-8').read()
-idx = open('index.html', encoding='utf-8').read()
-app_ids = re.findall(r"getElementById\('([^']+)'\)", app)
-idx_ids = re.findall(r'id="([^"]+)"', idx)
-dead = [x for x in app_ids if x not in idx_ids]
-print('DEAD IDS:', set(dead))
--- check_braces.py
+++
@@ -1,8 +0,0 @@
-text = open('app.js', encoding='utf-8').read()
-braces = 0
-for i, char in enumerate(text):
-    if char == '{': braces += 1
-    elif char == '}':
-        braces -= 1
-        if braces < 0: print('Negative braces at index', i)
-print('Final braces:', braces)
